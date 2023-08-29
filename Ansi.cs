c4c40cc14501a2e6df286b89b63db5318e6386ee
DynomiteWaffle <dynomitewaffle@protonmail.com>
2023-08-28 23:44:47 -0600

    Inproved Drawing To Screen

--- Ansi.cs
+++ Ansi.cs
@@ -4,9 +4,6 @@ class Ansi
 {
     int emptyColor = 0;
     int unknownColor = 1;
-    string background = "";
-    string foreground = "";
-
     List<List<int>> grid;
 
     List<VialSort.GameObjects.Color> colors;
@@ -16,35 +13,46 @@ class Ansi
         this.grid = new List<List<int>> {};
         // colors
         this.colors = Colors;
-        this.foreground = "\x1b[38;2;"+this.colors[(int)VialSort.GameObjects.ColorLocations.empty].red+";"+this.colors[(int)VialSort.GameObjects.ColorLocations.empty].green+";"+this.colors[(int)VialSort.GameObjects.ColorLocations.empty].blue+"m";
-        this.background = "\x1b[48;2;"+this.colors[(int)VialSort.GameObjects.ColorLocations.empty].red+";"+this.colors[(int)VialSort.GameObjects.ColorLocations.empty].green+";"+this.colors[(int)VialSort.GameObjects.ColorLocations.empty].blue+"m";
     }
     
     // TODO:BUG: doesn't draw correctly
     public void DrawVials(VialSort.GameObjects.Vial[] Vials,int index,int selected)
     {
+        int bufferWidth = Vials.Length+1;
+        string[] buffer = new string[Vials.Length*10];
+
+        foreach(int i in Enumerable.Range(0,buffer.Length))
+        {
+            buffer[i] = " ";
+        }
 
         if(selected >= 0)
         {
             if(selected == index)
             {
-                GoTo(selected*2+1,0);
-                DrawPixel(1,1);
+                // GoTo(selected*2+1,0);
+                SetAt(selected,0,bufferWidth,ref buffer,DrawPixel(1,1));
             }
             else
             {
-                GoTo(selected*2+1,0);
-                DrawPixel(0,1);
+                // GoTo(selected*2+1,0);
+                // DrawPixel(0,1);
+                SetAt(selected,0,bufferWidth,ref buffer,DrawPixel(0,1));
 
-                GoTo(index*2+1,0);
-                DrawPixel(1,0);
+
+                // GoTo(index*2+1,0);
+                // DrawPixel(1,0);
+                SetAt(index,0,bufferWidth,ref buffer,DrawPixel(1,0));
+
             }
 
         }
         else
         {
-            GoTo(index*2+1,0);
-            DrawPixel(1,0);
+            // GoTo(index*2+1,0);
+            // DrawPixel(1,0);
+            SetAt(index,0,bufferWidth,ref buffer,DrawPixel(1,0));
+
         }
         // background
         // │
@@ -58,70 +66,79 @@ class Ansi
             foreach(int i in Enumerable.Range(0,(int)Math.Ceiling(v.GetLenght()/2f)))
             {
 
-                // background
-                GoTo(cur*2,i+yoff);
-                Console.Write("│");
-
-                GoTo(cur*2+1,i+yoff);
-                DrawPixel(v.GetPos(i*2),v.GetPos(i*2+1));
-                // Console.Write(v.GetPos(i));
-                // GoTo(cur*2,i+yoff+v.GetLenght());
-                // SetColor(0,v.GetPos(i));
-                Console.Write("│");
-                // Console.ResetColor();
+                SetAt(cur,i+yoff,bufferWidth,ref buffer,DrawPixel(v.GetPos(i*2),v.GetPos(i*2+1)));
                 
             }
             cur ++;
             
         }
-        Console.WriteLine("");
+        // draw buffer
+        foreach(int i in Enumerable.Range(0,Vials[0].GetLenght()-1))
+        {
+            if(i==0)
+            {
+                Console.WriteLine(" "+string.Join(" ",buffer,i*bufferWidth,bufferWidth));
+
+            }
+            else
+            {
+                Console.WriteLine("│"+string.Join("│",buffer,i*bufferWidth,bufferWidth));
+            }
+
+
+        }
+
         foreach(int _ in Enumerable.Range(0,Vials.Count()*2+1)){Console.Write("▔");}
         Console.WriteLine("");
-        
-        // empty grid
-        // this.grid = new List<List<int>> {};
-        // // init grid
-        // foreach(int i in Enumerable.Range(0,Vials[0].GetLenght()))
-        // {
-        //     grid.Add(new List<int> {});
-        //     foreach(var v in Vials)
-        //     {
-        //         grid[grid.Count-1].Add(v.GetPos(i));
-        //     }
-        // }
-
-        // // print grid
-        // Console.WriteLine("");
-        // foreach(List<int> g in grid){foreach(int r in g){Console.Write("|"+r);}Console.WriteLine("|");}
-
 
     }
-    public void DrawPixel(int topColor,int bottomColor)
+    public string DrawPixel(int topColor,int bottomColor)
     {
+        string output = string.Empty;
         // pixel
         if(topColor != emptyColor)
         {
-            Console.Write("\x1b[38;2;"+this.colors[topColor].red+";"+this.colors[topColor].green+";"+this.colors[topColor].blue+"m"); // topcolor
+            output +=("\x1b[38;2;"+this.colors[topColor].red+";"+this.colors[topColor].green+";"+this.colors[topColor].blue+"m"); // topcolor
         }
         else
         {
-            Console.ForegroundColor = (ConsoleColor)0; //color of background
+            // Console.ForegroundColor = (ConsoleColor)0; //color of background
+            output +=("\x1b[30m");
         }
         if(bottomColor != emptyColor)
         {
-            Console.Write("\x1b[48;2;"+this.colors[bottomColor].red+";"+this.colors[bottomColor].green+";"+this.colors[bottomColor].blue+"m"); // bottomcolor
+            output +=("\x1b[48;2;"+this.colors[bottomColor].red+";"+this.colors[bottomColor].green+";"+this.colors[bottomColor].blue+"m"); // bottomcolor
         }
         else
         {
             // Console.BackgroundColor = (ConsoleColor)0; //color of background
+            // Console.Write("\x1b[40m");
+
         }
-        Console.Write("▀");
-        Console.Write("\x1b[0m"); // resetcolors
+        output +=("▀");
+        output +=("\x1b[0m"); // resetcolors
+
+        return output;
     }
     public void GoTo(int x,int y)
     {
         Console.CursorTop = y;
         Console.CursorLeft = x;
         // Console.Write("\x1b["+x+";"+y);
+    }
+    public void SetAt(int x,int y,int bufferWidth,ref string[] buffer, string Char)
+    {
+
+        if(x+(bufferWidth*y) > buffer.Length-1)
+        {
+            return;
+        }
+        if(x+(bufferWidth*y) < 0)
+        {
+            return;
+        }
+
+
+        buffer[x+(bufferWidth*y)] = Char;
     }
 }
